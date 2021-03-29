faa1d1c17195c0899fc2dce25b0c428d95f3cd72
ABA <aurelien.ba@ynov.com>
2021-03-29 20:16:14 +0200

    trash talk

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
+from math import random 
+
 class wordlist:
     def __init__(self,list):
         self.mainTab = []
@@ -7,19 +9,37 @@ class wordlist:
             'Your sister',
             'Your dad',
             'You',
-
+            'Your skill', 
+            'Your IQ',
+            
         ]
         verbTab = [
             'looks like',
             'is like',
             'eats',
+            ''
             'loves',
             'sniff',
-            ''
+            'is',
         ]   
         complementab = [
-            'a rotten fish',
+            'a fish',
             'a dog',
             'the floor',
             'a trash',
+            'an empty hole',
+            'idiot'
+        ]
+        endTab = [
+            'hairless',
+            'abandoned',
+            'lost',
+            'rotten',
+            'so stupid'
+        ]
+        linkTab = [
+            'and',
+            'also',
+            'too',
+            'moreover',
         ]
