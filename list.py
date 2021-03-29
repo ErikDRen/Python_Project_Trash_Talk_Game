e06c291cd67017fd42212bd548307d0cb03e13d0
ABA <aurelien.ba@ynov.com>
2021-03-29 18:32:51 +0200

    trash list

---
+++ list.py
@@ -0,0 +1,25 @@
+class wordlist:
+    def __init__(self,list):
+        self.mainTab = []
+        
+    def fillMainTab():
+        subjecTab = [
+            'Your sister',
+            'Your dad',
+            'You',
+
+        ]
+        verbTab = [
+            'looks like',
+            'is like',
+            'eats',
+            'loves',
+            'sniff',
+            ''
+        ]   
+        complementab = [
+            'a rotten fish',
+            'a dog',
+            'the floor',
+            'a trash',
+        ]
