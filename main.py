3e8b3da8a520bc817b64074e2660509c90930dd4
ABA <aurelien.ba@ynov.com>
2021-03-30 23:37:12 +0200

    main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from player import player
 from fct import *
 
+# --- REGISTER TIME ---
+
 #Player1 will enter his username 
 print('Player 1, Choose your username : ')
 username1 = str(input())
@@ -12,12 +14,27 @@ print('\n' +
 )
 getChar = str(input())
 
-#Will give the selected character to the player
+print(detect(getChar))
+
+
+#Will fill p1's info
 p1 = player(username1, giveChar(getChar))           
 
+#p2 enter his username
 print('Player2, Choose your username : ')
 username2 = str(input())
-p2 = player(username2)
+
+#Player1 will choose a character
+print('\n' +
+    username2 + 
+    ' choose your character : 1.Corentin 2.Juliette 3.Jean 4.Aurélie 5.Francis \nnote: Type the number of the player. For informations about the character type "?(number)"'
+)
+getChar2 = str(input())
+
+#Will fill p2's info
+p2 = player(username2, giveChar(getChar2))
+
+
 print(p1.name + ' VS ' + p2.name + ' FIGHT !')
 
 Turnp1 = 'Player 1, your turn.'
