f0f5ab04c4f34b77287278306584cf463438bfc8
ABA <aurelien.ba@ynov.com>
2021-03-30 23:36:31 +0200

    detect p2 avoir

--- fct.py
+++ fct.py
@@ -1,6 +1,8 @@
 #Will give the selecter character to the player
 def giveChar(stre):
+
     selectedChar = ''
+
     if stre == '1':
         selectedChar += 'Corentin'
     elif stre == '2':
@@ -14,16 +16,35 @@ def giveChar(stre):
 
     return selectedChar  
 
+#Will give characters' informations
+def giveCharInfo(stre):
+
+    informations = ''
+
+    if stre == '1':
+        informations += 'Corentin wins more points using words of subjectab'
+    elif stre == '2':
+        informations += 'Juliette wins more points using words of vertab'
+    elif stre == '3':
+        informations += 'Jean wins more points using words of complementab'
+    elif stre == '4':
+        informations  += 'Aurélie wins more points using words of endtab'       
+    elif stre == '5':
+        informations += 'Francis wins more points using words of linktab'         
+
+    return informations    
+
+
 #Will do the difference if player want info of character
 #J'arrive pas a append putain
-def infoChar(stre):
-    perso1 = 'Corentin gets a bonus'
-    sentence = ''
-    for i in stre:
-        if i == '?':
-            continue
-            if i == '1':
-                sentence += perso1
-    return sentence            
-                
-print(infoChar(getChar))     
+def detect(stre):
+
+    selected = ''
+
+    if len(stre) == 1:
+        selected += giveChar(stre)
+    elif len(stre) == 1:
+        for i in stre:
+            if i[0] == '?':
+               selected += giveCharInfo(i[1])
+    return selected
