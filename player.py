efd57157ebc7246e25ff8ec8da1735b97eef45d3
ABA <aurelien.ba@ynov.com>
2021-03-30 23:00:56 +0200

    info player

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 class player:
-    def __init__(self,name): 
+    def __init__(self,name,perso): 
         self.name = name
         self.points = 0
+        self.perso = perso
 
     def __str__(self):
         return self.name
