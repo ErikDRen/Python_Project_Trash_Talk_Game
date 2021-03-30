3b02af709c23e0b8c4dbb44081316c3334d2ba56
ABA <aurelien.ba@ynov.com>
2021-03-30 23:01:14 +0200

    main

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from player import player
+from fct import *
 
+#Player1 will enter his username 
 print('Player 1, Choose your username : ')
 username1 = str(input())
-p1 = player(username1)
+
+#Player1 will choose a character
+print('\n' +
+    username1 + 
+    ' choose your character : 1.Corentin 2.Juliette 3.Jean 4.Aurélie 5.Francis \nnote: Type the number of the player. For informations about the character type "?(number)"'
+)
+getChar = str(input())
+
+#Will give the selected character to the player
+p1 = player(username1, giveChar(getChar))           
+
 print('Player2, Choose your username : ')
 username2 = str(input())
 p2 = player(username2)
@@ -14,5 +26,4 @@ Turnp2 = 'Player 2, your turn.'
 # 2. Choose verb
 # 3. Choose situation
 # 4. Choose end
-def round():
 
